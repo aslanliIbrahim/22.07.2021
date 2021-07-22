fa29f912cb3737142d846ebdf5bb4a9cb34ccad6
aslanliIbrahim <ibrahimra@code.edu.az>
2021-07-22 22:06:03 +0400

    scrooldown

---
+++ Task/index.js
@@ -0,0 +1,25 @@
+function Scrolldown(){
+    var red = document.getElementById("red")
+    window.scroll({
+        top: red.offsetTop,
+        behavior:"smooth",
+    })
+}
+
+function Scroll(){
+    var blue = document.getElementById("blue")
+    window.scroll({
+        top:blue.offsetTop,
+        behavior:"smooth"
+    })
+}
+
+function Scrolldownn(){
+    var grenn = document.getElementById("green")
+    window.scroll({
+        top:grenn.offsetTop,
+        behavior:"smooth"
+    })
+    
+}
+
